4bf003219bd821478537b1d0aede9c6a8da57aa9
ZaviiNet <trancefx@live.co.uk>
2022-12-10 08:47:48 +0000

    Updated README.md Added index.php and keys and emails csv files

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Key-Claimer
+
+Key Claimer is a simple 1 script tool to claim keys from a csv file and log the emails of those who have claimed the keys.
+
+## Installation
+
+1. Download the latest release from the [releases page].
+2. Create a csv file with the keys you want to claim making sure that each key is on a new line (You can also use the example csv file provided).
+3. Create a csv file named email.csv and leave it blank (You can also use the example csv file provided).
+4. Edit the index.php file and make sure that you have the correct path to the keys.csv file and the email.csv file, You may wish to customize The page title and the page heading.
+5. Edit the reCaptcha 'SITEKEY_API' otherwise the reCaptcha will not work.
+6. Upload the files to your web server.
+
+## Usage
+
+1. Navigate to the index.php file on your web server.
+2. Enter the email you want to claim the key with.
+3. Press 'Claim Key' it will first check your email is valid and then check if you have already claimed a key. If you have not claimed a key it will then check if there are any keys left to claim. If there are keys left to claim it will then claim the key and add your email to the email.csv file.
+4. If you have already claimed a key it will tell you that you have already claimed a key.
+5. If there are no keys left to claim it will tell you that there are no keys left to claim.
+6. If you have entered an invalid email it will tell you that you have entered an invalid email.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Disclaimer
+
+1. This tool is not affiliated with any game or company. This tool is for educational purposes only.
+2. This tool uses the reCaptcha API from Google.
+3. This tool is not responsible for GDPR compliance.
+
+**Warning this tool is not GDPR compliant and you should not use this tool if you are not GDPR compliant as it uses an accessible csv file to store emails and keys. If you wish to be GDPR compliant you should use a database to store the emails and keys.**
---
+++ index.php
@@ -0,0 +1,121 @@
+﻿<!DOCTYPE html>
+<html>
+   <head>
+      <title>Example Title</title>
+      <style>
+    body {
+         padding: 20px;
+      }
+h1 {
+         color: navy;
+         font-family: sans-serif;
+         font-size: 24px;
+      }
+         /* Style the claim button and Keys */
+         #claim-button {
+         font-size: 20px;
+         padding: 10px 20px;
+         background-color: #4CAF50;
+         color: white;
+         cursor: pointer;
+         }
+.error {
+    color: red;
+  }
+  .success {
+    color: green;
+  }
+form {
+    width: 400px;
+    margin: 0 auto;
+    font-family: Arial;
+  }
+  input[type=email], input[type=submit] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+  }
+  input[type=submit] {
+    background-color: #4CAF50;
+    color: white;
+    cursor: pointer;
+  }
+p {
+    font-family: arial;
+}
+      </style>
+      <script src="https://www.google.com/recaptcha/api.js"></script>
+   </head>
+   <body>
+      <center><h1>Example Title Key Claimer</h1>
+      <p>Keys are 1 per User, a valid email address is required</p>
+
+<?php
+
+// Read the steam keys from a CSV file and store them in an array
+$tickets = array();
+$file = fopen('keys.csv', 'r');
+while (($line = fgetcsv($file)) !== FALSE) {
+  $keys[] = $line[0];
+}
+fclose($file);
+
+// Read the email addresses from a CSV file and store them in an array
+$emails = array();
+$file = fopen('emails.csv', 'r');
+while (($line = fgetcsv($file)) !== FALSE) {
+  $emails[] = $line[0];
+}
+fclose($file);
+echo "<center><p>There are  " . count($emails) . " Keys Claimed</p></center>";
+
+// When the user clicks the "Claim Key" button,
+// check that they have entered a valid email address
+if (isset($_POST['claim_key'])) {
+  $email = $_POST['email'];
+  if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (in_array($email, $emails)) {
+      echo "<center><h1></br>You have already claimed a Key with this email address<h1></center>";
+    } else {
+      $randomIndex = array_rand($keys);
+      $key = $keys[$randomIndex];
+      echo "<center><p>Here is your Key $key </p></center>";
+
+      // Remove the selected key from the array
+      unset($keys[$randomIndex]);
+
+      // Overwrite the keys CSV file with the updated array of keys
+      $file = fopen('keys.csv', 'w');
+      foreach ($keys as $key) {
+        fputcsv($file, array($ticket));
+      }
+      fclose($file);
+
+      // Append the email address to the email addresses CSV file
+      $file = fopen('emails.csv', 'a');
+      fputcsv($file, array($email));
+      fclose($file);
+    }
+  } else {
+    echo "<center><h1></br>Please enter a valid email address<h1></center>";
+  }
+}
+
+?>
+<div id="form-area">
+<form method="post">
+  <input type="email" name="email" placeholder="Enter your email">
+<div class="g-recaptcha" data-sitekey="SITEKEY_API" data-callback="onSubmit" data-size="invisible">
+  <input type="submit" name="claim_ticket" value="Claim Key">
+
+</form>
+<script>
+      function onSubmit(token) {
+        document.getElementById("form-area").submit();
+      }
+    </script>
+</div>
+ </body>
+   </html>
+
